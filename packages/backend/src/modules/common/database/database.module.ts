cf072f87cf6b0f90a2a5c43e4ac5b0e4d1e853cc
Clément Delestre <clementdelestre@gmail.com>
2024-07-04 00:21:16 +0200

    feat(both): add modal to show cats without vote

--- packages/backend/src/modules/common/database/database.module.ts
+++ packages/backend/src/modules/common/database/database.module.ts
@@ -13,7 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       synchronize: process.env.DB_SYNCHRONIZE === 'true',
-      logging: ['error'],
+      logging: ['error', 'query'],
       cache: false,
       autoLoadEntities: true,
     }),
---
+++ packages/frontend/src/app/modules/score/components/score/components/novote/novote.component.ts
@@ -0,0 +1,71 @@
+import { Component, ComponentRef, TemplateRef, ViewChild, signal } from '@angular/core';
+import { ApiService } from '../../../../../../core/http/api.service';
+import { ModalService } from '../../../../../../core/services/modal.service';
+import { Order } from '../../../../../../shared/constants/order.constant';
+import { PageMetaDto } from '../../../../../../shared/dto/page-meta.dto';
+import { PageOptionsDto } from '../../../../../../shared/dto/page-options.dto';
+import { RankedCat } from '../../../../models/ranked-cat.model';
+import { PageDto } from '../../../../../../shared/dto/page.dto';
+import { firstValueFrom } from 'rxjs';
+import { getCatPictureUrl } from '../../../../../../shared/tools/url.tool';
+
+@Component({
+  selector: 'app-novote',
+  standalone: true,
+  imports: [],
+  templateUrl: './novote.component.html',
+  styleUrl: './novote.component.scss'
+})
+export class NovoteModalComponent {
+
+  public getCatPictureUrl = getCatPictureUrl;
+  isLoading = signal(false);
+
+  noVoteModalComponent?: ComponentRef<any>
+  @ViewChild('noVoteModalBody') noVoteModalBody!: TemplateRef<any>;
+
+  pageMeta?: PageMetaDto;
+
+  pagination: PageOptionsDto = {
+    page: 1,
+    take: 10,
+    order: Order.DESC,
+  };
+
+  rankedCats = signal<RankedCat[]>([]);
+
+  constructor(private readonly apiService: ApiService, private readonly modalService: ModalService) { }
+
+  ngOnInit() {
+  } 
+
+  showNoVoteModal() {
+    this.rankedCats.set([]);
+    this.pagination.page = 1;
+    this.getNoVote();
+  }
+
+  getNoVote() {
+    this.isLoading.set(true);
+
+    firstValueFrom(
+      this.apiService.get<PageDto<RankedCat>>('score/novote', this.pagination),
+    ).then((page) => {
+      console.log(page);
+      this.rankedCats.update((v) => [...v, ...page.data]);
+      this.pageMeta = page.meta;
+      this.isLoading.set(false);
+
+      if(!this.noVoteModalComponent){
+        this.noVoteModalComponent = this.modalService.showModal(`Chats sans vote (${ page.meta.itemCount })`, this.noVoteModalBody);
+        this.noVoteModalComponent.onDestroy(() => { this.noVoteModalComponent = undefined})
+      }
+    });
+  }
+
+  loadMore() {
+    this.pagination.page++;
+    this.getNoVote();
+  }
+
+}
